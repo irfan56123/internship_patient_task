278678278e7c52cf51e5e56f09ecae5842bd833e
irfan56123 <ia9412738@gmail.com>
2025-08-14 14:04:28 +0530

    added patient-info

--- src/app/api/doctors/[id]/route.ts
+++ src/app/api/doctors/[id]/route.ts
@@ -2,8 +2,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { doctors } from '@/data/doctors';
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  const doctor = doctors.find((doc) => doc.id === Number(params.id));
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+
+  const doctor = doctors.find((doc) => doc.id === Number(id));
 
   if (!doctor) {
     return NextResponse.json({ error: 'Doctor not found' }, { status: 404 });
